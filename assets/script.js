f5d853330e2237a78bb3e80792a6c7f45685a2d1
K1tanaK0mbat <kkeayouth@gmail.com>
2023-06-24 12:10:34 -0500

    added random topic functions

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,28 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-
-for(var x = 0; x < topics.length; x++) {
+function listTopic() {
+  for(var x = 0; x < topics.length; x++) {
   console.log(topics[x]);
  }
- /*
-if (topic === 'HTML') {
+}
+
+function selectTopic() {
+  if (randomTopic === 'HTML') {
  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
  console.log("Let's study JavaScript!");
 } else {
  console.log('Please try again!');
 }
-*/
+}
+
+console.log('Here are the topics we learned through Prework:');
+listTopic();
+
+console.log('Which topic should we study first?');
+selectTopic();
